0b93374bca82e7bb970abc0bca7cb56d6eb5f761
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2026-01-29 22:04:21 +0530

    added map

---
+++ MapInJava/src/HashmapLaunch.java
@@ -0,0 +1,53 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class HashmapLaunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer,String> hm1 = new HashMap<>();
+		
+		hm1.put(3,"Java");
+		hm1.put(2,"SpringBoot");
+		hm1.put(1,"Ankit");
+		hm1.put(null,"Sarthak");
+		hm1.put(4,null);
+		
+		
+		System.out.println(hm1);
+		
+		System.out.println("....................");
+		
+		Collection<String>values = hm1.values();
+		Iterator<String> itr = values.iterator();
+		
+		while(itr.hasNext()) {
+			System.out.print(itr.next()+" ");
+		}
+		
+		System.out.println("....................");
+		
+		Set<Integer> keys = hm1.keySet();
+		Iterator<Integer> itr2 = keys.iterator();
+		while(itr2.hasNext()) {
+			System.out.print(itr2.next()+" ");
+		}
+		
+		System.out.println("....................");
+		
+		Set entry = hm1.entrySet();
+		Iterator itr3 = entry.iterator();
+		while(itr3.hasNext()) {
+			Map.Entry pair = (Entry) itr3.next();
+			System.out.println(pair.getKey()+" "+pair.getValue());
+		}
+		
+
+	}
+
+}
---
+++ MapInJava/src/LinkedHashmapJava.java
@@ -0,0 +1,53 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class LinkedHashmapJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedHashMap<Integer,String> hm1 = new LinkedHashMap<>();
+		
+		hm1.put(3,"Java");
+		hm1.put(2,"SpringBoot");
+		hm1.put(1,"Ankit");
+		hm1.put(null,"Sarthak");
+		hm1.put(4,null);
+		
+		
+		System.out.println(hm1);
+		
+		System.out.println("....................");
+		
+		Collection<String>values = hm1.values();
+		Iterator<String> itr = values.iterator();
+		
+		while(itr.hasNext()) {
+			System.out.print(itr.next()+" ");
+		}
+		
+		System.out.println("....................");
+		
+		Set<Integer> keys = hm1.keySet();
+		Iterator<Integer> itr2 = keys.iterator();
+		while(itr2.hasNext()) {
+			System.out.print(itr2.next()+" ");
+		}
+		
+		System.out.println("....................");
+		
+		Set entry = hm1.entrySet();
+		Iterator itr3 = entry.iterator();
+		while(itr3.hasNext()) {
+			Map.Entry pair = (Entry) itr3.next();
+			System.out.println(pair.getKey()+" "+pair.getValue());
+		}
+		
+
+	}
+
+}
