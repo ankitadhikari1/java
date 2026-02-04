f1496609e61f5323293b341dad2042bf99732ea7
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2026-02-04 13:26:38 +0530

    added java basics folder for basic topics

---
+++ javaBasics/src/javaBasics/ObjectClass.java
@@ -0,0 +1,44 @@
+package javaBasics;
+
+
+interface PayemntInterface{
+	public void doPayment(double amount);
+}
+
+//class Razorpay implements PayemntInterface{
+//	@Override
+//	public void doPayment(double amount) {
+//		System.out.println("payment done using razorpay :"+amount);
+//	}
+//}
+
+class Paypal implements PayemntInterface{
+	@Override
+	public void doPayment(double amount) {
+		System.out.println("payment done using paypal :"+amount);
+	}
+}
+
+class CheckOut{
+	private PayemntInterface obj ;
+	public void pay(PayemntInterface paymentGateway , double amount ) {
+		this.obj = paymentGateway;
+		obj.doPayment(amount);
+	}
+}
+
+
+public class ObjectClass {
+
+	public static void main(String[] args) {
+	
+		
+		CheckOut obj = new CheckOut();
+		obj.pay(new Paypal(), 500);
+		
+		
+		
+
+	}
+
+}
