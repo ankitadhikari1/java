dea68db47f5c9f553d1ec6dbb21f2e3f6ea1af63
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2026-02-14 21:32:02 +0530

    redirection in java using redirect

---
+++ RedirectionInServlet/src/main/java/FirstServlet.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("control in first Servlet");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/SecondServlet");
+		rd.include(request, response);
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>this is first servlet</h1>");
+		System.out.println("control again in first Servlet");
+	}
+
+}
